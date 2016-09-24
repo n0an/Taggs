563334fdce76b9ae72b818f7ad3630a6f3c1aefd
n0an <nantog@gmail.com>
2016-09-24 11:07:02 +0300

    keyboard notifications added

--- Taggs/NewPostViewController.swift
+++ Taggs/NewPostViewController.swift
@@ -11,15 +11,21 @@ import Photos
 
 class NewPostViewController: UIViewController {
     
+    // MARK: - OUTLETS
+    
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var currentUserProfileImageView: UIImageView!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var postContentTextView: UITextView!
     @IBOutlet weak var postImageView: UIImageView!
     
+    // MARK: - BAR CUSTOMIZATION
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+    
+    // MARK: - viewDidLoad
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +36,36 @@ class NewPostViewController: UIViewController {
         currentUserProfileImageView.layer.cornerRadius = currentUserProfileImageView.bounds.width/2
         currentUserProfileImageView.layer.masksToBounds = true
         
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(NewPostViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(NewPostViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 
+    // MARK: - HELPER METHODS
+    
+    func presentImagePicker() {
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        imagePicker.allowsEditing = false
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: - ACTIONS
     
     @IBAction func pickFeaturedImageClicked(_ sender: UITapGestureRecognizer) {
         
@@ -56,25 +90,33 @@ class NewPostViewController: UIViewController {
     }
     
     
-    func presentImagePicker() {
-        let imagePicker = UIImagePickerController()
+    
+    // MARK: - TEXT VIEW HANDLERS
+    
+    func keyboardWillHide(notification: NSNotification) {
+        print("keyboardWillHide")
         
-        imagePicker.delegate = self
+        self.postContentTextView.contentInset = UIEdgeInsets.zero
+        self.postContentTextView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
         
-        imagePicker.allowsEditing = false
+        let userInfo = notification.userInfo ?? [:]
         
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            imagePicker.sourceType = .camera
-        } else {
-            imagePicker.sourceType = .photoLibrary
-        }
+        let keyboardSize = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue.size
         
-        present(imagePicker, animated: true, completion: nil)
+        self.postContentTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        self.postContentTextView.scrollIndicatorInsets = self.postContentTextView.contentInset
+
     }
     
     
 }
 
+
+// MARK: - UIImagePickerControllerDelegate
 
 extension NewPostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
